effd4614b4ca4411a2273506e458798298b78bbd
myoung kyu lee <myoungkyu.lee@kellogg.northwestern.edu>
2023-06-04 00:32:33 +0000

    changed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["username"]}"
+        redirect_to "/places"
+      else
+        flash["notice"] = "Unsuccessful login. Check your password again"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Unseccessful login. Check your email again"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to "/sessions/new"
   end
 end
   
